7068d48dd2c3acf7d78afa07ea793255bd6c41f0
Crowdbotics <team@crowdbotics.com>
2021-01-13 19:16:01 +0000

    remove package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -4,7 +4,6 @@ export const installed_blueprints = [
 { name: 'Camera2192554', human_name: 'Camera 2', access_route: 'Camera2192554', icon: 'camera'},
 { name: 'SignIn23192553', human_name: 'Sign In 2 3', access_route: 'SignIn23192553'},
 { name: 'BlankScreen1192552', human_name: 'Blank Screen 1', access_route: 'BlankScreen1192552'},
-{ name: 'BlankScreen0192551', human_name: 'Blank Screen 0', access_route: 'BlankScreen0192551'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -9,7 +9,6 @@ import CalendarView1192555Navigator from '../features/CalendarView1192555/naviga
 import Camera2192554Navigator from '../features/Camera2192554/navigator';
 import SignIn23192553Navigator from '../features/SignIn23192553/navigator';
 import BlankScreen1192552Navigator from '../features/BlankScreen1192552/navigator';
-import BlankScreen0192551Navigator from '../features/BlankScreen0192551/navigator';
 
 /**
  * new navigators can be imported here
@@ -22,7 +21,6 @@ CalendarView1192555: { screen: CalendarView1192555Navigator },
 Camera2192554: { screen: Camera2192554Navigator },
 SignIn23192553: { screen: SignIn23192553Navigator },
 BlankScreen1192552: { screen: BlankScreen1192552Navigator },
-BlankScreen0192551: { screen: BlankScreen0192551Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
